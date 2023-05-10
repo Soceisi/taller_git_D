684a011378172b5264689cf4d7b9af916c0cacba
Soceisi <univallesoceisi@gmail.com>
2023-05-10 16:00:34 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace grupo_D
+{
+    internal class Program
+    {
+         void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int result = Sum(2, 3);
+        }
+
+
+        public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}

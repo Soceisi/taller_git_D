0264d201f1ef026601fc6abfd279306f5a4da919
Soceisi <univallesoceisi@gmail.com>
2023-05-10 16:06:33 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
             int result = Sum(2, 3);
+            string nombreCompleto = NombreCompleto("Andy", "Guzman");
         }
 
 
         public int Sum(int n1, int n2)
         {
             return n1 + n2;
+        }
+
+        public string NombreCompleto(string name, string lastName)
+        {
+            return name + lastName;
         }
     }
 }
